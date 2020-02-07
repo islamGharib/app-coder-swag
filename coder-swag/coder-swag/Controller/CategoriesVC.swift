dea249e2a271e02074e1ab06fd6114068eea043e
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-02-08 00:27:11 +0200

    Creating the category table view

--- coder-swag/coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/coder-swag/Controller/CategoriesVC.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesVC: UIViewController {
+    @IBOutlet weak var categoryTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Islam Gharib on 2/8/20.
+//  Copyright © 2020 Gharib. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
