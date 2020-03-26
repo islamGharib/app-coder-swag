4b88054ceda3cba6a5ed101771fd73087be5567d
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-03-26 18:53:27 +0200

    Creating Products VC

---
+++ coder-swag/coder-swag/Controller/ProductsVC.swift
@@ -0,0 +1,19 @@
+//
+//  ProductsVC.swift
+//  coder-swag
+//
+//  Created by Islam Gharib on 3/26/20.
+//  Copyright © 2020 Gharib. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
